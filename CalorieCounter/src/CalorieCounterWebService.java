24ddfb9c6f95b6c0b6bed6ade9d1321c7dedbf88
AAlarifi <101008206+AAlarifi@users.noreply.github.com>
2023-03-10 15:06:02 +0000

    Added /user route and /sedentary
    /food works and has been tested in postman (body -> x-www-form-urlencoded)

--- CalorieCounter/src/CalorieCounterWebService.java
+++ CalorieCounter/src/CalorieCounterWebService.java
@@ -8,7 +8,7 @@ import spark.Route;
 public class CalorieCounterWebService {
 
 	public static void main(String[] args) {
-		
+
 		port(8088);
 
 		// Simple route so you can check things are working...
@@ -21,20 +21,42 @@ public class CalorieCounterWebService {
 				}
 			}
 		});
-		
-		// Need a front-end to work correctly
+
+		// Has been successfully tested in postman
 		// Accessible via http://localhost:8088/food in your browser
 		post("/food", (request, response) -> {
-		    String name = request.queryParams("name");
-		    int calories = Integer.parseInt(request.queryParams("calories"));
-		    try (DataBase db = new DataBase()) {
-		        db.createFood(name, calories);
-		    }
-		    response.redirect("/test");
-		    return null;
+			String name = request.queryParams("name");
+			int calories = Integer.parseInt(request.queryParams("calories"));
+			try (DataBase db = new DataBase()) {
+				db.createFood(name, calories);
+			}
+			response.redirect("/test");
+			return null;
 		});
 
-		
+		//
+		// Accessible via http://localhost:8088/user in your browser
+		post("/user", (request, response) -> {
+			int weightInKg = Integer.parseInt(request.queryParams("weightInKg"));
+			int heightInCm = Integer.parseInt(request.queryParams("heightInCm"));
+			int ageInYears = Integer.parseInt(request.queryParams("ageInYears"));
+			try (DataBase db = new DataBase()) {
+				db.menBMR(weightInKg, heightInCm, ageInYears);
+			}
+			return "a user has been created";
+		});
+
+		// Accessible via http://localhost:8088/sedentary in your browser
+		// could be user/sedentary ????
+		post("/sedentary", new Route() {
+			@Override
+			public Object handle(Request request, Response response) throws Exception {
+				try (DataBase db = new DataBase()) {
+					return "Number of Entries: " + db.Sedentary();
+				}
+			}
+		});
+
 	}
 
 }
