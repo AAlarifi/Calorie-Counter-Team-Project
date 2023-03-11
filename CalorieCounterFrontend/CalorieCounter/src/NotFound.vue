aaf921eb11e8e2c2f9a045a4a67b4f291f976c85
Bramwell Simpson <bramwellsimpson1@gmail.com>
2023-03-11 19:00:24 +0000

    Add styling to 404 page

--- CalorieCounterFrontend/CalorieCounter/src/NotFound.vue
+++ CalorieCounterFrontend/CalorieCounter/src/NotFound.vue
@@ -1,3 +1,14 @@
 <template>
-    <h1>error 404...</h1>
+    <v-app>
+        <v-main class="bg-grey-darken-4">
+            <v-container style="height: 100vh;" class="align-center d-flex">
+                <v-row justify="center">
+                    <v-col cols="12" lg="6">
+                        <h1 class="text-h1 text-center">Error 404...</h1>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
+    <!-- <h1>error 404...</h1> -->
 </template>
