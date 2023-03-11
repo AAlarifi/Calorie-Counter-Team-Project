93729b9c8eab444199278cbbb5efb4c31df15817
Bramwell Simpson <bramwellsimpson1@gmail.com>
2023-03-11 19:01:38 +0000

    Change home page file name (for now)

--- CalorieCounterFrontend/CalorieCounter/src/router/index.js
+++ CalorieCounterFrontend/CalorieCounter/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 
-import Home from "../views/pages/Home.vue"
+import Home from "../views/pages/Home-2.vue"
 import Food from "../views/pages/Food.vue"
 import NotFound from "../NotFound.vue"
 
