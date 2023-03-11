68eec97f9357d518aebb5f7721d2c436c12e1905
AAlarifi <workernumber20@gmail.com>
2023-03-11 18:12:23 +0000

    fixing issue with files

---
+++ CalorieCounterFrontend/CalorieCounter/src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+
+import Home from "../views/pages/Home.vue"
+import Food from "../views/pages/Food.vue"
+import NotFound from "../NotFound.vue"
+
+
+const routes = [
+    { path: "/", component: Home },
+    {path: "/food", component: Food},
+    { path: "/:pathMatch(.*)*", component: NotFound}
+]
+//the error route should be the last in the array..
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+export default router;
---
+++ CalorieCounterFrontend/CalorieCounter/src/views/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/food">Food</router-link>
+    <router-view/>
+    <h1>Home page </h1>
+  </div>
+</template>
+
+<script>
+import  router  from "../router/index.js";
+
+export default {
+  name: "App",
+  router,
+};
+</script>
