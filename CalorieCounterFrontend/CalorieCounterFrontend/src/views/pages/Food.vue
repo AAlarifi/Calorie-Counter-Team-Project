23a657f9eeba0241b6651dea3d006b8228d17d2f
AAlarifi <workernumber20@gmail.com>
2023-04-05 15:11:38 +0100

    Refractor code

--- CalorieCounterFrontend/CalorieCounterFrontend/src/views/pages/Food.vue
+++ CalorieCounterFrontend/CalorieCounterFrontend/src/views/pages/Food.vue
@@ -1,10 +1,7 @@
 <template>
   <v-app>
     <v-main class="bg-grey-darken-4">
-      <v-container
-        style="height: 100vh; max-height: 100vh"
-        class="align-center d-flex"
-      >
+      <v-container style="height: 100vh; max-height: 100vh" class="align-center d-flex">
         <v-row justify="center">
           <v-col cols="12" lg="6">
             <v-card elevation="10">
@@ -14,16 +11,11 @@
               <v-card-text>
                 <v-form @submit.prevent="submitData">
                   <v-text-field v-model="food" label="Food"></v-text-field>
-                  <v-text-field
-                    v-model="calories"
-                    label="Calories"
-                  ></v-text-field>
+                  <v-text-field v-model="calories" label="Calories"></v-text-field>
                 </v-form>
               </v-card-text>
               <v-card-actions>
-                <v-btn @click="submitData" block variant="outlined"
-                  >Submit</v-btn
-                >
+                <v-btn @click="submitData" block variant="outlined">Submit</v-btn>
               </v-card-actions>
             </v-card>
             <v-snackbar v-model="snackbar">
@@ -51,17 +43,11 @@
                 </v-form>
               </v-card-text>
               <v-card-actions>
-                <v-btn @click="searchFoodButton" block variant="outlined"
-                  >Search</v-btn
-                >
+                <v-btn @click="searchFoodButton" block variant="outlined">Search</v-btn>
               </v-card-actions>
             </v-card>
             <v-list>
-              <v-list-item
-                v-for="(result, index) in searchResults"
-                :key="index"
-                @click="selectResult(result)"
-              >
+              <v-list-item v-for="(result, index) in searchResults" :key="index" @click="selectResult(result)">
                 <v-list-item-title>{{ result.name }}</v-list-item-title>
                 <v-list-item-subtitle>{{
                   result.calories
